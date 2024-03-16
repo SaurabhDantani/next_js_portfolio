b19d53862407852735d2797dbb5cf9cc0fd3a0f5
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-03-16 08:22:41 +0000

    add type effect

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import { IndexTypeEffect } from "@/ui/TypewriterEffect/IndexTypeEffetc";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -57,7 +58,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.h1
+      {/* <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -67,7 +68,9 @@ export default function Intro() {
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-      </motion.h1>
+      </motion.h1> */}
+
+      <IndexTypeEffect />
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
